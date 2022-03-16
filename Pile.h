18b8163bff606f36236e68625e4cc08f3c16bdd5
yaya752 <yassine76440@gmail.com>
2022-03-16 16:39:19 +0100

    structure finie

--- Pile.h
+++ Pile.h
@@ -1,1 +1,22 @@
-g
+#ifndef PILE_H
+#define PILE_H
+
+//création du type générique
+
+typedef struct type_générique
+{
+	int element;
+}T;
+
+typedef struct Maillon
+{
+	T valeur;
+	struct maillon * suivant
+}maillon_t;
+
+typedef struct Pile
+{
+	maillon * tete;
+}pile_t;
+
+#endif
